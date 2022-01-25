9f6c438148c9eff66ad54f9feb1496b2c39371ea
Karthick.S <karthick.s@tranetechnologies.com>
2022-01-25 17:22:24 +0530

    account changes

--- bin/cdk-codepipeline.ts
+++ bin/cdk-codepipeline.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { CdkCodepipelineStack } from '../lib/cdk-codepipeline-stack';
 
 const app = new cdk.App();
-const myaws = { account: '713792433226', region: 'us-east-1' };
+const myaws = { account: '029909986479', region: 'us-east-1' };
 new CdkCodepipelineStack(app, 'CdkCodepipelineStack', {
   env:myaws,
   description:"test stack for cicd",
